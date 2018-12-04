a38f7b04d48aea0bf14e4d695a530350c14196fe
Jonathan Van Dam <mustafolins@gmail.com>
2018-12-03 22:42:51 -0700

    Changed letters to uppercase

--- go-mind-example.go
+++ go-mind-example.go
@@ -59,27 +59,27 @@ var (
 )
 
 func main() {
-	m := mind.New(0.3, 100000, 6, "sigmoid")
+	m := mind.New(0.3, 1000000, 6, "sigmoid")
 	m.Learn([][][]float64{
-		{f, mapletter('f')},
-		{e, mapletter('e')},
-		{d, mapletter('d')},
-		{c, mapletter('c')},
-		{b, mapletter('b')},
-		{a, mapletter('a')},
+		{f, mapletter('F')},
+		{e, mapletter('E')},
+		{d, mapletter('D')},
+		{c, mapletter('C')},
+		{b, mapletter('B')},
+		{a, mapletter('A')},
 	})
 
 	result := m.Predict([][]float64{
 		character( // slightly resembles an 'A'
-			"..####." +
-				"#.....#" +
-				"#.....#" +
-				"#.#..##" +
-				"#.....#" +
-				"#.....#" +
-				"#.....#")})
+			"######." +
+				"#......" +
+				"#......" +
+				"#.##.#." +
+				"#......" +
+				"#..#..." +
+				"#......")})
 	log.Println(result)
-	fmt.Println(closestletter(result.At(0, 0)))
+	fmt.Println(closestasciiart(result.At(0, 0)))
 }
 func character(chars string) []float64 {
 	flt := make([]float64, len(chars))
@@ -94,45 +94,45 @@ func character(chars string) []float64 {
 }
 
 func mapletter(letter byte) []float64 {
-	if letter == 'a' {
+	if letter == 'A' {
 		return []float64{0.16}
 	}
-	if letter == 'b' {
+	if letter == 'B' {
 		return []float64{0.32}
 	}
-	if letter == 'c' {
+	if letter == 'C' {
 		return []float64{0.48}
 	}
-	if letter == 'd' {
+	if letter == 'D' {
 		return []float64{0.64}
 	}
-	if letter == 'e' {
+	if letter == 'E' {
 		return []float64{0.80}
 	}
-	if letter == 'f' {
+	if letter == 'F' {
 		return []float64{1.0}
 	}
 	return []float64{0.0}
 }
 
-func closestletter(result float64) string {
+func closestasciiart(result float64) string {
 	if result >= 0 && result < 0.24 {
-		return "a"
+		return "A"
 	}
 	if result >= 0.24 && result < 0.40 {
-		return "b"
+		return "B"
 	}
 	if result >= 0.40 && result < 0.56 {
-		return "c"
+		return "C"
 	}
 	if result >= 0.56 && result < 0.72 {
-		return "d"
+		return "D"
 	}
 	if result >= 0.72 && result < 0.88 {
-		return "e"
+		return "E"
 	}
 	if result >= 0.88 && result <= 1.0 {
-		return "f"
+		return "F"
 	}
 	return ""
 }
